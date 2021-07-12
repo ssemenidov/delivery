6f2072021affa82f3980ce66eb69a0d3717e9099
ssemenidov <23sergey03@gmail.com>
2021-07-12 11:56:40 +0800

    adress page

--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
-
+import SearchBar from '../components/SearchBar';
 function Address() {
-  return <div>address page</div>;
+  return (
+    <div className='p-4 flex flex-col justify-between h-screen'>
+      <div className='flex flex-col h-full justify-center align-center'>
+        <SearchBar />
+      </div>
+      <div className='py-4'>
+        <button className='btn-orange '>Продолжить</button>
+        <div className='text-grey text-sm py-2'>
+          <p>
+            Нажимая кнопку Продолжить Вы соглашаетесь с
+            <a href='#' className='text-orange underline'>
+              {' '}
+              Условиями продоставления услуги{' '}
+            </a>
+            и
+            <a href='#' className='text-orange underline'>
+              {' '}
+              Правилами пользования сервисом
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Address;
