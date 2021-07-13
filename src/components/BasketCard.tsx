f81ff10f1aed757b5e50022deeab695813619e7b
ssemenidov <23sergey03@gmail.com>
2021-07-13 11:40:36 +0800

    basket

---
+++ src/components/BasketCard.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {CardType} from '../interfaces';
+interface BasketCard extends CardType {
+  deleteCard: (x: String) => void;
+}
+function BasketCard({
+  id,
+  title,
+  descr,
+  url,
+  price,
+  mass,
+  deleteCard,
+}: BasketCard) {
+  return (
+    <div className='flex flex-row rounded-xl relative'>
+      <div className=' '>
+        <div
+          className='min-w-full w-20 h-20  bg-center bg-no-repeat bg-cover rounded-l-lg'
+          style={{
+            backgroundImage: `url(${url})`,
+          }}
+        ></div>
+      </div>
+      <div className='flex justify-between px-3 flex-col h-20 '>
+        <div className=''>
+          <div className='text-black1 text-sm font-medium pb-1'>{title}</div>
+          <div
+            className='text-gray1 text-xs  overflow-y-hidden'
+            style={{fontSize: '10px', lineHeight: '11px'}}
+          >
+            {descr}
+          </div>
+        </div>
+        <div className='flex flex-row w-full justify-between'>
+          <div className=' text-black1 whitespace-nowrap text-xs self-center'>
+            {mass} гр
+          </div>
+          <div className='whitespace-nowrap text-orange1 text-sm font-medium'>
+            {price} руб.
+          </div>
+        </div>
+      </div>
+      <div className='absolute top-0 right-0'>
+        <button onClick={() => {}} className='p-2 focus:outline-none'>
+          <svg
+            width='10'
+            height='10'
+            viewBox='0 0 10 10'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fill-rule='evenodd'
+              clip-rule='evenodd'
+              d='M0.27933 8.37179C-0.09311 8.74424 -0.0931098 9.34809 0.27933 9.72054C0.651769 10.093 1.25561 10.093 1.62805 9.72054L4.99994 6.34861L8.37195 9.72067C8.74439 10.0931 9.34823 10.0931 9.72067 9.72067C10.0931 9.34822 10.0931 8.74437 9.72067 8.37192L6.34866 4.99986L9.7204 1.62808C10.0928 1.25563 10.0928 0.651779 9.7204 0.279334C9.34795 -0.0931111 8.74411 -0.0931114 8.37167 0.279334L4.99994 3.65112L1.62833 0.279462C1.25589 -0.092983 0.652045 -0.0929828 0.279606 0.279462C-0.0928338 0.651907 -0.0928341 1.25576 0.279606 1.62821L3.65121 4.99986L0.27933 8.37179Z'
+              fill='#B9B9B9'
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BasketCard;
--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -1,7 +1,139 @@
 import React from 'react';
-
+import {useHistory} from 'react-router-dom';
+import tomYam from '../assets/img/tom-yam.png';
+import BasketCard from '../components/BasketCard';
+import {CardType} from '../interfaces';
+const dishes: CardType[] = [
+  {
+    id: '1',
+    title: 'Фо-Бо',
+    descr:
+      'Мясной бульон на говядине, рисовая лапша, мраморная говядина, базилик, мята, кинза.',
+    url: tomYam,
+    price: 320,
+    mass: 420,
+  },
+  {
+    id: '2',
+    title: 'Том Ям',
+    descr: 'Куриный бульон, грибы, креветки, острая паста Том Ям',
+    url: tomYam,
+    price: 290,
+    mass: 350,
+  },
+  {
+    id: '3',
+    title: 'Том Ям',
+    descr: 'Куриный бульон, грибы, креветки, острая паста Том Ям',
+    url: tomYam,
+    price: 290,
+    mass: 350,
+  },
+  {
+    id: '4',
+    title: 'Том Ям',
+    descr: 'Куриный бульон, грибы, креветки, острая паста Том Ям',
+    url: tomYam,
+    price: 290,
+    mass: 350,
+  },
+  {
+    id: '4',
+    title: 'Том Ям',
+    descr: 'Куриный бульон, грибы, креветки, острая паста Том Ям',
+    url: tomYam,
+    price: 290,
+    mass: 350,
+  },
+  {
+    id: '4',
+    title: 'Том Ям',
+    descr: 'Куриный бульон, грибы, креветки, острая паста Том Ям',
+    url: tomYam,
+    price: 290,
+    mass: 350,
+  },
+  {
+    id: '4',
+    title: 'Том Ям',
+    descr: 'Куриный бульон, грибы, креветки, острая паста Том Ям',
+    url: tomYam,
+    price: 290,
+    mass: 350,
+  },
+];
 function Basket() {
-  return <div>basket page</div>;
+  const history = useHistory();
+  const deleteCard = (id: String) => {};
+  return (
+    <div className=' flex flex-col  min-h-screen align-center w-full '>
+      <div className='p-4'>
+        <div className='py-2 flex'>
+          <div className='mr-4'>
+            <button
+              onClick={() => history.goBack()}
+              className='    focus:outline-none'
+            >
+              <svg
+                width='16'
+                height='11'
+                viewBox='0 0 16 11'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  fill-rule='evenodd'
+                  clip-rule='evenodd'
+                  d='M0.195262 5.59086C0.131344 5.52694 0.083119 5.45327 0.0505858 5.37464C0.0200275 5.30095 0.00230584 5.22059 0.000209987 5.13635C7.02739e-05 5.13073 0 5.1251 0 5.11945C0 4.94884 0.0650874 4.77822 0.195262 4.64805L4.64805 0.195262C4.9084 -0.0650874 5.33051 -0.0650873 5.59086 0.195262C5.85121 0.455612 5.85121 0.877722 5.59086 1.13807L2.27614 4.45279H14.8353C15.2035 4.45279 15.502 4.75126 15.502 5.11945C15.502 5.48764 15.2035 5.78612 14.8353 5.78612H2.27614L5.59086 9.10083C5.85121 9.36118 5.85121 9.78329 5.59086 10.0436C5.33051 10.304 4.9084 10.304 4.64805 10.0436L0.195262 5.59086Z'
+                  fill='#2C2C2C'
+                />
+              </svg>
+            </button>
+          </div>
+          <div className='text-xl font-medium'>Корзина</div>
+        </div>
+        <div className='py-2 mb-4'>
+          <div className='text-sm bg-white flex items-center rounded-md shadow-lg'>
+            <input
+              className='rounded w-full py-3 px-6 text-gray1  focus:outline-none'
+              id='search'
+              type='text'
+              placeholder='Марьиной Рощи, 2'
+              readOnly
+            />
+          </div>
+        </div>
+        <div className='overflow-y-auto'>
+          {dishes.map((value, index) => (
+            <div className='mb-6'>
+              <BasketCard {...value} deleteCard={deleteCard}></BasketCard>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div
+        className='sticky bottom-0  text-sm bg-white p-4  rounded-md'
+        style={{boxShadow: ' 0px -5px 15px rgba(84, 84, 84, 0.1)'}}
+      >
+        <div className='text-black1 flex justify-between'>
+          <div className=''>Итого с доставкой:</div>
+          <div className=''>1168 руб.</div>
+        </div>
+        <div className='text-gray1 flex justify-between mb-5'>
+          <div className=''>Время доставки:</div>
+          <div className=''>≈ 40 мин.</div>
+        </div>
+        <button
+          className='btn-orange mb-4'
+          onClick={() => {
+            alert('Спасибо!');
+          }}
+        >
+          Заказать в Яндекс Еде
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Basket;
